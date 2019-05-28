45144bc4118712ad08ad81865aa813a1a3b6d6f8
iliastic <mesbahiilias1@gmail.com>
2019-05-28 19:44:18 +0200

    fixed the onload & onhoover

---
+++ assets/JS/doge.js
@@ -0,0 +1,25 @@
+document.getElementById("doge").addEventListener("mouseover", woofwoof);
+document.getElementById("doge").addEventListener("mouseout", woof);
+document.getElementById("doge").addEventListener("click", picture);
+
+function picture() {
+  if (document.getElementById("pic").style.display === "none") {
+    document.getElementById("pic").style.display = "block";
+    console.log("lol");
+  } else {
+    document.getElementById("pic").style.display = "none";
+    console.log("lol2");
+  }
+}
+
+function woofwoof() {
+  document.getElementById("doge").innerHTML = "woof woof!";
+}
+
+function woof() {
+  document.getElementById("doge").innerHTML = "woof!";
+}
+
+function loadPage() {
+  document.getElementById("doge").innerHTML = "woof";
+}
